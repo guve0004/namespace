8de74b6acd87e3599467c452e2e76e274804fd30
guve0004 <43075480+guve0004@users.noreply.github.com>
2018-10-25 22:41:48 -0400

    Create namespace.js

---
+++ namespace.js
@@ -0,0 +1,30 @@
+var GUVE0004 = {
+
+	init: function() {
+
+		let div = document.createElement(“div”);
+
+		div.setAttribute(“class”, “box”);
+		div.textContent = “guve0004 ";
+
+
+		div.addEventListener(“click”, () => {
+			div.style.borderColor = “blue”;
+			div.style.backgroundColor = “aqua”;
+		});
+
+		div.addEventListener(“mouseover”, () => {
+			div.classList.toggle(“highlight”);
+		});
+
+		div.addEventListener(“mouseout”, () => {
+			div.classList.toggle(“highlight”);
+			div.removeAttribute(“style”);
+		});
+
+		let boxes = document.querySelector("#boxes");
+		if (boxes) {
+			boxes.append(div);
+		}
+	}
+};
